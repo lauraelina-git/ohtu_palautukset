2daa51f81221bbe900e549cf4c90689e207e280f
Laura Hainari <laura@Mac.Home>
2025-10-31 09:07:05 +0200

    week 2 exercise 4

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,22 +1,32 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url: str):
+        self.url = url
+    
+    def get_players(self):
+        response = requests.get(self.url)
+        response.raise_for_status()
+        data = response.json()
+        return [Player(p) for p in data]
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality: str):
+        filtered = [p for p in self.players if p.nationality == nationality]
+        return sorted(filtered, key=lambda p: p.total_points(), reverse=True)
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = [Player(p) for p in response]
-
-    # Suodatetaan suomalaiset
-    finnish_players = [p for p in players if p.nationality == "FIN"]
-
-    #järjestetään:
-    finnish_sorted = sorted(finnish_players, key=lambda p: p.total_points(), reverse=True)
-
-    print("Players from FIN:")
-    for player in finnish_sorted:
+    for player in players:
         print(player)
-
 
 if __name__ == "__main__":
     main()
